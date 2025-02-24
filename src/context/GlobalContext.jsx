6bddbfa3efca3a966ef5534eb23b7c23ced6beee
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-24 15:30:38 +0100

    Feat: aggiunto env, e inseriti fetch per chiamate axios

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,12 +1,42 @@
-import { useContext, createContext } from "react";
+import axios from "axios";
+import { useContext, createContext, useState } from "react";
 
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-  const value = {};
+
+const api_url= import.meta.env.VITE_API_URL;
+
+const [immobili, setImmobili] = useState([])
+const [immobile, setImmobile] = useState(null)
+
+const fetchImmobili = () =>{
+  axios.get(api_url)
+  .then(res =>{
+    setImmobili(res.data)
+  })
+  .catch(err => console.log(err))
+}
+
+const fetchImmobile = (id) =>{
+  axios.get(`${api_url}/${id}`)
+  .then(res =>{
+    setImmobile(res.data)
+  })
+  .catch(err => console.log(err))
+}
+
+  const value = {
+    immobili, 
+    fetchImmobili,
+    immobile,
+    fetchImmobile
+  };
 
   return (
-    <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider value={value}>
+      {children}
+    </GlobalContext.Provider>
   );
 };
 
