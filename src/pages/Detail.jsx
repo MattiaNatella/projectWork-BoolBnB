11b1d97a6053740529517c7a55db0c6b5357c236
carlosvillaar <carlosvillar004@gmail.com>
2025-02-24 15:34:38 +0100

    feat: pagina dettaglio migliorata

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Detail = () => {
-  return <div>Dettaglio immobile</div>;
+  return (
+    <>
+      <div className="container">
+        <h3>Titolo immobile</h3>
+        <img
+          className="w-100 py-3"
+          src="https://placehold.co/600x400/EEE/31343C"
+          alt="placeholder"
+        />
+        <p>numero cuoricini</p>
+        <p>numero recensioni</p>
+        <div className="details py-5">
+          <p>descrizione dell'immobile</p>
+          <p>metri quadrati</p>
+          <p>via dell'immobile</p>
+          <p>stanze dell'immobile</p>
+          <p>bagni dell'immobile</p>
+        </div>
+
+        <div className="recensioni">
+          <h1>recensioni</h1>
+          <p>recensione</p>
+          <p>recensione</p>
+          <p>recensione</p>
+          <p>recensione</p>
+          <p>recensione</p>
+          <p>recensione</p>
+          <p>recensione</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Detail;
