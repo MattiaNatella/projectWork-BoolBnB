7a4981d67f85bf07aa82b1d8a8c2faa6d7654a1d
LordAnthrax94 <andreagi1994@gmail.com>
2025-02-24 16:17:37 +0100

    fix: aggiunto stile alla navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ const Navbar = () => {
   return (
     <div>
       <nav className="navbar">
-        <div className="container-fluid bg-black">
+        <div className="container-fluid">
           <a className="navbar-brand" href="/">
             <img
               src="https://img.freepik.com/vettori-premium/eccezionale-monogramma-bn-o-bnb-o-nb-lettere-logo-design_873531-453.jpg?w=996"
               alt="Logo"
               width="100"
               height="100"
-              className="d-inline-block align-text-top"
+              className="d-inline-block align-text-top imgLogo"
             />
           </a>
-          <a href="/nuovo-immobile" className="text-dark fw-bolder">
+          <a href="/nuovo-immobile" className="text-dark bg-success">
             Aggiungi un immobile
           </a>
         </div>
