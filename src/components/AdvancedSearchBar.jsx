9c826c31848b1185fde914a19e0e2a3404e80c72
carlosvillaar <carlosvillar004@gmail.com>
2025-02-25 17:08:20 +0100

    fix: ricerca avanzata funzionante

--- src/components/AdvancedSearchBar.jsx
+++ src/components/AdvancedSearchBar.jsx
@@ -1,15 +1,14 @@
-import { useGlobalContext } from "../context/GlobalContext"
-import { useEffect, useState } from "react"
-
+import { useGlobalContext } from "../context/GlobalContext";
+import { useEffect, useState } from "react";
 
 const AdvancedSearchBar = () => {
+  const { handleAdvancedSearch, tipologie, fetchTipologie } =
+    useGlobalContext();
+  const [stanze, setStanze] = useState("");
+  const [bagni, setBagni] = useState("");
+  const [tipologia, setTipologia] = useState("");
 
-  const {handleAdvancedSearch, tipologie, fetchTipologie} = useGlobalContext()
-  const [stanze, setStanze] = useState('');
-  const [bagni, setBagni] = useState('');
-  const [tipologia, setTipologia] = useState('');
-
-  useEffect(()=>{ fetchTipologie()}, [fetchTipologie])
+  useEffect(() => fetchTipologie, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,42 +17,44 @@ const AdvancedSearchBar = () => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input 
-          className="form-control me-2"
-          type="number"
-          placeholder="inserisci numero di stanze"
-          aria-label="Search"
-          onChange={(e) => setStanze(e.target.value)} 
-          min={1}
-          max={10}
-              
-        />
+      <input
+        className="form-control me-2"
+        type="number"
+        placeholder="inserisci numero di stanze"
+        aria-label="Search"
+        onChange={(e) => setStanze(e.target.value)}
+        min={1}
+        max={10}
+      />
 
-      <input 
-          className="form-control me-2"
-          type="number"
-          placeholder="inserisci numero di bagni"
-          aria-label="Search"
-          onChange={(e) => setBagni(e.target.value)}
-          min={1}
-          max={10}       
-        />
+      <input
+        className="form-control me-2"
+        type="number"
+        placeholder="inserisci numero di bagni"
+        aria-label="Search"
+        onChange={(e) => setBagni(e.target.value)}
+        min={1}
+        max={10}
+      />
 
-      <select 
-        className="form-select" 
-        aria-label="Default select example" 
-        onChange={(e) => setTipologia(e.target.value)}>
-
+      <select
+        className="form-select"
+        aria-label="Default select example"
+        onChange={(e) => setTipologia(e.target.value)}
+      >
         <option value="">Seleziona la tipologia</option>
-        {Array.isArray(tipologie) && tipologie?.map((tipologia) => (
-          <option key={tipologia?.id} value={tipologia?.id}>{tipologia?.descrizione}
-          </option>
-        ))}
-        
+        {Array.isArray(tipologie) &&
+          tipologie?.map((tipologia) => (
+            <option key={tipologia?.id} value={tipologia?.id}>
+              {tipologia?.descrizione}
+            </option>
+          ))}
       </select>
-      <button type="submit" className="btn btn-primary mt-2">Cerca</button>
+      <button type="submit" className="btn btn-primary mt-2">
+        Cerca
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default AdvancedSearchBar
+export default AdvancedSearchBar;
