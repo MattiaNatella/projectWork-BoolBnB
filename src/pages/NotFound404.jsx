d638385b38f7f4a654901cb6171eb6161eef18a1
carlosvillaar <carlosvillar004@gmail.com>
2025-02-25 15:53:41 +0100

    feat: aggiornata pagina 404

--- src/pages/NotFound404.jsx
+++ src/pages/NotFound404.jsx
@@ -1,9 +1,61 @@
 const NotFound404 = () => {
-  return(
-  <div className="container">
-    <h1 className="text-danger text-center">ERRORE 404</h1>
-  </div>  
-  ) 
+  const styles = {
+    container: {
+      position: "relative",
+      height: "80vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+    },
+    notfound: {
+      maxWidth: "520px",
+      width: "100%",
+      lineHeight: "1.4",
+    },
+    notfound404: {
+      position: "relative",
+      height: "200px",
+      margin: "0 auto 20px",
+      fontSize: "236px",
+      fontWeight: "200",
+      color: "#211b19",
+      textTransform: "uppercase",
+      paddingBottom: "250px",
+    },
+    notfoundText: {
+      fontSize: "28px",
+      fontWeight: "400",
+      textTransform: "uppercase",
+      color: "#211b19",
+      background: "#fff",
+      padding: "10px 5px",
+      display: "inline-block",
+    },
+    link: {
+      display: "inline-block",
+      fontWeight: "700",
+      textDecoration: "none",
+      color: "#fff",
+      textTransform: "uppercase",
+      padding: "13px 23px",
+      fontSize: "18px",
+      transition: "0.2s all",
+    },
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.notfound}>
+        <div style={styles.notfound404}>Oops!</div>
+        <div style={styles.notfoundText}>404 - Pagina non trovata</div>
+        <br />
+        <a href="/" style={styles.link} className="btn btn-primary">
+          Torna alla Homepage
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound404;
