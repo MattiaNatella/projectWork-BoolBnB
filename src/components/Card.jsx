d1844e47c5a57c60f604b38346966eed07b2d9af
carlosvillaar <carlosvillar004@gmail.com>
2025-02-25 17:14:42 +0100

    fix: aggiunta commento

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,31 @@
 import { Link } from "react-router-dom";
 
-const Card = ({immobile}) =>{
+const Card = ({ immobile }) => {
   return (
     <div className="d-flex justify-content-center mt-2">
       <div className="houseCard card">
-        <img src={immobile.immagine} className="card-img-top" alt={immobile.descrizione_immobile} />
+        <img
+          src={immobile.immagine}
+          className="card-img-top"
+          alt={immobile.descrizione_immobile}
+        />
         <div className="card-body">
           <h5 className="card-title">{immobile.descrizione_immobile}</h5>
-          <p className="card-text">{immobile.stanze}</p>
+          <p className="card-text">Stanze: {immobile.stanze}</p>
           <div>
-          <Link to={`/dettaglio-immobile/${immobile.id}`} className="btn btn-success">Vedi dettagli</Link>
+            <Link
+              to={`/dettaglio-immobile/${immobile.id}`}
+              className="btn btn-success"
+            >
+              Vedi dettagli
+            </Link>
           </div>
         </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Card;
+
+//salviamo andrea!
