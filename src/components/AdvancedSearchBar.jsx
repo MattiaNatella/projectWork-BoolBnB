ec57eeac73d47410458c90f6066c7eb9cb18ee03
carlosvillaar <carlosvillar004@gmail.com>
2025-02-25 18:05:53 +0100

    fix: ricerca avanzata aggiornata e funzionante

--- src/components/AdvancedSearchBar.jsx
+++ src/components/AdvancedSearchBar.jsx
@@ -1,59 +1,97 @@
-import { useGlobalContext } from "../context/GlobalContext"
-import { useEffect, useState } from "react"
-
+import { useGlobalContext } from "../context/GlobalContext";
+import { useEffect, useState } from "react";
 
 const AdvancedSearchBar = () => {
+  const { handleAdvancedSearch, tipologie, fetchTipologie } =
+    useGlobalContext();
 
-  const {handleAdvancedSearch, tipologie, fetchTipologie} = useGlobalContext()
-  const [stanze, setStanze] = useState('');
-  const [bagni, setBagni] = useState('');
-  const [tipologia, setTipologia] = useState('');
+  const resetSearch = {
+    stanze: "",
+    bagni: "",
+    tipologia: 0,
+  };
 
-  useEffect(()=>{ fetchTipologie()}, [fetchTipologie])
+  const [filters, setFilters] = useState(resetSearch);
+  const [error, setError] = useState("");
+
+  const validate = () => {
+    if (!filters.stanze || !filters.bagni || !filters.tipologia) return false;
+    return true;
+  };
+
+  useEffect(() => {
+    fetchTipologie();
+  }, [fetchTipologie]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    handleAdvancedSearch({ stanze, bagni, tipologia });
+
+    if (!validate()) {
+      setError(
+        "Controlla bene i dati, potrebbe esserci qualche errore, ricordati i dati sono obbligatori"
+      );
+      return;
+    }
+
+    handleAdvancedSearch(filters);
+    setFilters(resetSearch);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prev) => ({ ...prev, [name]: value }));
+    setError("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input 
-          className="form-control me-2"
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control me-2 my-2"
           type="number"
           placeholder="inserisci numero di stanze"
           aria-label="Search"
-          onChange={(e) => setStanze(e.target.value)} 
+          onChange={handleChange}
+          name="stanze"
+          value={filters.stanze}
           min={1}
           max={10}
-              
         />
 
-      <input 
-          className="form-control me-2"
+        <input
+          className="form-control me-2 my-2"
           type="number"
           placeholder="inserisci numero di bagni"
           aria-label="Search"
-          onChange={(e) => setBagni(e.target.value)}
+          onChange={handleChange}
+          name="bagni"
+          value={filters.bagni}
           min={1}
-          max={10}       
+          max={10}
         />
 
-      <select 
-        className="form-select" 
-        aria-label="Default select example" 
-        onChange={(e) => setTipologia(e.target.value)}>
+        <select
+          className="form-select my-2"
+          aria-label="Default select example"
+          onChange={handleChange}
+          name="tipologia"
+          value={filters.tipologia}
+        >
+          <option value="">Seleziona la tipologia</option>
+          {Array.isArray(tipologie) &&
+            tipologie?.map((tipologia) => (
+              <option key={tipologia?.id} value={tipologia?.id}>
+                {tipologia?.descrizione}
+              </option>
+            ))}
+        </select>
+        <h6 className="text-center text-danger">{error}</h6>
+        <button type="submit" className="btn btn-primary mt-2 w-100 mb-5">
+          Cerca
+        </button>
+      </form>
+    </>
+  );
+};
 
-        <option value="">Seleziona la tipologia</option>
-        {Array.isArray(tipologie) && tipologie?.map((tipologia) => (
-          <option key={tipologia?.id} value={tipologia?.id}>{tipologia?.descrizione}
-          </option>
-        ))}
-        
-      </select>
-      <button type="submit" className="btn btn-primary mt-2">Cerca</button>
-    </form>
-  )
-}
-
-export default AdvancedSearchBar
+export default AdvancedSearchBar;
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,32 +1,30 @@
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "../context/GlobalContext";
 
-import{ Link } from "react-router-dom"
-import { useGlobalContext } from "../context/GlobalContext"
+const Searchbar = () => {
+  const { handleSearch } = useGlobalContext();
 
-
-const Searchbar = () =>{
-  
-  const {handleSearch} = useGlobalContext()
-  
   return (
-
-    
-        <div className="d-flex mt-2"> 
-          <div>
-            <input 
-              className="form-control searchBar"
-              type="search"
-              placeholder="Cerca un immobile"
-              aria-label="Search"
-              onChange={handleSearch}       
-            />      
-          </div>
-          <div>
-            <Link to={'/ricerca-avanzata'} className="btn btn-primary ms-2">Cerca immobile</Link>
-          </div>
-        </div>
-      
-    
-  )
-}
+    <div className="d-flex mt-2">
+      <div>
+        <input
+          className="form-control searchBar"
+          type="search"
+          placeholder="Cerca un immobile nella tua città"
+          aria-label="Search"
+          onChange={handleSearch}
+        />
+      </div>
+      <div>
+        {/* <button type="submit" className="btn btn-primary ms-2">
+          Ricerca
+        </button> */}
+        <Link to={"/ricerca-avanzata"} className="btn btn-primary ms-2">
+          Ricerca Avanzata
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Searchbar;
