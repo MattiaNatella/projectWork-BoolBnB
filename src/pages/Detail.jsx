9c3d2b7e65230dd3c027758537d52aafecbaaf56
carlosvillaar <carlosvillar004@gmail.com>
2025-02-28 11:35:51 +0100

    fix: modificata la pagina dettaglio

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,7 +3,7 @@ import { useGlobalContext } from "../context/GlobalContext";
 import { useParams } from "react-router-dom";
 import ModalReview from "../components/ModalReview";
 import ReviewCard from "../components/Reviews";
-
+import Rating from "../components/Rating";
 
 const Detail = () => {
   const { fetchImmobile, immobile } = useGlobalContext();
@@ -12,33 +12,50 @@ const Detail = () => {
 
   return (
     <>
-      <div className="container">
-        <h3 className="mt-5">{immobile?.descrizione_immobile}</h3>
-        <div className="d-flex">
+      <div className="container py-5">
+        <div className="card-top d-flex gap-5 justify-content-between  ">
           <img
-            className="w-50 py-3"
             src={immobile?.immagine}
-            alt="placeholder"
+            className="w-50"
+            alt="immagine immobile"
           />
-          <div className="details my-3 fs-5 ms-3">
-
-            <p><strong>Metri Quadrati:</strong> {immobile?.metri_quadrati}</p>
-            <p><strong>Indirizzo:</strong> {immobile?.indirizzo}</p>
-            <p><strong>Stanze:</strong> {immobile?.stanze}</p>
-            <p><strong>Bagni:</strong> {immobile?.bagni}</p>
-            <p> {immobile?.testo_descrittivo}</p>
+          <div>
+            <div className="d-flex justify-content-between mb-3">
+              <h3>{immobile?.descrizione_immobile}</h3>
+              <span className="fs-4">
+                <Rating valutazione={immobile?.media_valutazione} />
+              </span>
+            </div>
+            <p className="fw-bolder fs-5">€ {immobile?.prezzo_notte} / notte</p>
+            <div className="details fs-5 fw-light ">
+              <p> {immobile?.testo_descrittivo}</p>
+              <p>
+                <strong className="fw-bold pe-2">Metri Quadrati:</strong>{" "}
+                {immobile?.metri_quadrati} m²
+              </p>
+              <p>
+                <strong className="fw-bold pe-2">Indirizzo:</strong>{" "}
+                {immobile?.indirizzo}
+              </p>
+              <p>
+                <strong className="fw-bold pe-2">Stanze:</strong>{" "}
+                {immobile?.stanze}
+              </p>
+              <p>
+                <strong className="fw-bold pe-2">Bagni:</strong>{" "}
+                {immobile?.bagni}
+              </p>
+            </div>
+            <div className="recensioni mt-5">
+              <h1>Recensioni:</h1>
+              <ModalReview immobile={immobile} />
+            </div>
+            <div className="row row-cols-3 m-2">
+              {immobile?.reviews.map((recensione) => (
+                <ReviewCard review={recensione} key={recensione.id} />
+              ))}
+            </div>
           </div>
-        </div>
-
-
-
-
-        <div className="recensioni mt-5">
-          <h1>Recensioni:</h1>
-          <ModalReview />
-        </div>
-        <div className="row row-cols-3 m-2">
-         {immobile?.reviews.map(recensione => (<ReviewCard review={recensione} key={recensione.id} />))} 
         </div>
       </div>
     </>
